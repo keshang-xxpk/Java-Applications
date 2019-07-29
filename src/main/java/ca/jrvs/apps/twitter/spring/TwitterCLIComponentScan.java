1e63ec39b328aafda11a44d746479181d71c4b32
keshang-xxpk <52050219+keshang-xxpk@users.noreply.github.com>
2019-07-29 15:50:32 -0400

    Update TwitterCLIComponentScan.java

--- src/main/java/ca/jrvs/apps/twitter/spring/TwitterCLIComponentScan.java
+++ src/main/java/ca/jrvs/apps/twitter/spring/TwitterCLIComponentScan.java
@@ -1,4 +1,16 @@
-package ca.jrvs.apps.spring;
+package ca.jrvs.apps.twitter.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@Configuration
+@ComponentScan(value = "ca.jrvs.apps.twitter")
 public class TwitterCLIComponentScan {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(
+        TwitterCLIComponentScan.class);
+    TwitterCLIRunner runner = context.getBean(TwitterCLIRunner.class);
+    runner.run(args);
+  }
 }
